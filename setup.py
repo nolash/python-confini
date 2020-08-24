5db75d07238f0f7129d73006db8fc52031f5ec33
nolash <dev@holbrook.no>
2020-08-24 23:49:07 +0200

    Remove license file from file list

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
         name='confini',
-        version='0.0.3',
+        version='0.0.4',
         description='Parse, verify and merge all ini files in a single directory',
         author='Louis Holbrook',
         author_email='dev@holbrook.no',
+        license='GPL3',
         packages=[
             'confini',
         ],
         scripts = [
             'scripts/parse.py',
             ],
-        data_files = [('', ['LICENSE.txt'])],
         url='https://holbrook.no',
         )
