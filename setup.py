b7e0f4f8e2e0256b19856d633c8fa32efad0de9a
nolash <dev@holbrook.no>
2021-02-19 21:44:41 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ f.close()
 
 setup(
         name='confini',
-        version='0.3.6rc2',
+        version='0.3.6rc3',
         description='Parse, verify and merge all ini files in a single directory',
         author='Louis Holbrook',
         author_email='dev@holbrook.no',
